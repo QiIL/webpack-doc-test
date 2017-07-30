a22d47de7ec8ceb5f50c7539499d350ca82c39c2
qsy <qsy_skx_code@qq.com>
2017-07-30 23:28:22 +0800

    test to doc HRM

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import _ from 'lodash';
+import printMe from './print';
+import './asset/css/style.css';
+// the Icon is an src
+import Icon from './asset/img/icon.jpg'
+
+if (module.hot) {
+  module.hot.accept('./print.js', function () {
+    console.log('Accepting the updated printMe module!');
+    printMe();
+  })
+}
+
+function component () {
+  var element = document.createElement('div');
+  var btn = document.createElement('button');
+
+  element.innerHTML = _.join(['Hello', 'Webpack'], '  ');
+  
+  btn.innerHTML = 'Click me and check the console';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+  element.classList.add('hello');
+  
+  //add the img to our existing div
+  var myIcon = new Image();
+  myIcon.src = Icon;
+  element.style.height = "200px"
+  //element.appendChild(myIcon); // add an <img src = myIcon>
+  return element;
+}
+
+document.body.appendChild(component());
---
+++ src/print.js
@@ -0,0 +1,6 @@
+export default function printMe() {
+  // console.log('I get called from print.js');
+  // console.log('I get called from print.js!');
+  console.log('updateing print.js ...');
+  console.log('updateaaa');
+}
